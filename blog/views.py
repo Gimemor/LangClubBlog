a57d44664c0d33ce9ef5f6d8b7b28dd4d2306d46
Oleg Brezhnev <oleg-423@yandex.ru>
2018-05-07 19:51:22 +0300

    Pagination and style fixes

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from blog.models import Post
+from django.core.paginator import Paginator
 
+PAGE_SIZE = 4
 
 # Create your views here.
 def index(request):
     # Retrieve all the posts from the user's this person is following.
-    posts = Post.objects.all()
+    allposts = Post.objects.all()
+    paginator = Paginator(allposts, PAGE_SIZE)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
     # This will render the posts.
     return render(request, 'post.html', {'posts': posts})
 
