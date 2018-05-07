c331926079480d3c0b945f90422f7ee867fa59b2
Oleg Brezhnev <oleg-423@yandex.ru>
2018-05-07 17:02:27 +0300

    Add blog entries

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.TextField(default='')
+    description = models.TextField(default='')
+    post = models.TextField(default='')
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __unicode__(self):
+        return "%s" % self.post
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Post
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("You're at blog main page")
+    # Retrieve all the posts from the user's this person is following.
+    posts = Post.objects.all()
+    # This will render the posts.
+    return render(request, 'post.html', {'posts': posts})
 
 
